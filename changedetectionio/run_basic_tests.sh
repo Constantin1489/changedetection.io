909aabb704dddc7f4ddc463b8f1d8f271bad9466
Constantin Hong <hongconstantin@gmail.com>
2023-10-17 05:07:22 +0900

    speed test

--- changedetectionio/run_basic_tests.sh
+++ changedetectionio/run_basic_tests.sh
@@ -11,28 +11,29 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-find tests/test_*py -type f|while read test_name
-do
-  echo "TEST RUNNING $test_name"
-  pytest $test_name
-done
+#find tests/test_*py -type f|while read test_name
+#do
+#  echo "TEST RUNNING $test_name"
+#  pytest $test_name
+#done
+pytest -s tests/test_xpath_selector.py
 
 echo "RUNNING WITH BASE_URL SET"
 
 # Now re-run some tests with BASE_URL enabled
 # Re #65 - Ability to include a link back to the installation, in the notification.
 export BASE_URL="https://really-unique-domain.io"
-pytest tests/test_notification.py
+#pytest tests/test_notification.py
 
 
 # Re-run with HIDE_REFERER set - could affect login
 export HIDE_REFERER=True
-pytest tests/test_access_control.py
+#pytest tests/test_access_control.py
 
 # Re-run a few tests that will trigger brotli based storage
 export SNAPSHOT_BROTLI_COMPRESSION_THRESHOLD=5
-pytest tests/test_access_control.py
-pytest tests/test_notification.py
-pytest tests/test_backend.py
-pytest tests/test_rss.py
-pytest tests/test_unique_lines.py
+#pytest tests/test_access_control.py
+#pytest tests/test_notification.py
+#pytest tests/test_backend.py
+#pytest tests/test_rss.py
+#pytest tests/test_unique_lines.py
