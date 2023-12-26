8537cd354e6260229c06015a420d9dcdfa560663
Constantin Hong <hongconstantin@gmail.com>
2023-12-26 23:31:57 +0900

    test: conftest.py

--- changedetectionio/tests/conftest.py
+++ changedetectionio/tests/conftest.py
@@ -56,3 +56,19 @@ def app(request):
        
     request.addfinalizer(teardown)
     yield app
+
+import pytest
+from loguru import logger
+from _pytest.logging import LogCaptureFixture
+@pytest.fixture
+def caplog(caplog: LogCaptureFixture):
+    handler_id = logger.add(
+        caplog.handler,
+        format="{message}",
+        level=0,
+        filter=lambda record: record["level"].no >= caplog.handler.level,
+        enqueue=False, # Set to 'True' if your test is spawning child processes.
+    )
+    yield caplog
+    logger.remove(handler_id)
+
