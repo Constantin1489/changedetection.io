f27ea18fb5027ecfa9251d5064086cb6fb2a27e5
Constantin Hong <hongconstantin@gmail.com>
2023-12-27 18:30:24 +0900

    conftest/test: fix import order

--- changedetectionio/tests/conftest.py
+++ changedetectionio/tests/conftest.py
@@ -11,6 +11,28 @@ import os
 
 global app
 
+import pytest
+from loguru import logger
+from _pytest.logging import LogCaptureFixture
+@pytest.fixture
+def caplog(caplog: LogCaptureFixture):
+    handler_id = logger.add(
+        caplog.handler,
+        format="{message}",
+        level=0,
+        filter=lambda record: record["level"].no >= caplog.handler.level,
+        enqueue=False, # Set to 'True' if your test is spawning child processes.
+    )
+    yield caplog
+    logger.remove(handler_id)
+
+@pytest.fixture
+def reportlog(pytestconfig):
+    logging_plugin = pytestconfig.pluginmanager.getplugin("logging-plugin")
+    handler_id = logger.add(logging_plugin.report_handler, format="{message}")
+    yield
+    logger.remove(handler_id)
+
 
 def cleanup(datastore_path):
     import glob
@@ -56,29 +78,4 @@ def app(request):
        
     request.addfinalizer(teardown)
     yield app
-
-import pytest
-from loguru import logger
-from _pytest.logging import LogCaptureFixture
-@pytest.fixture
-def caplog(caplog: LogCaptureFixture):
-    handler_id = logger.add(
-        caplog.handler,
-        format="{message}",
-        level=0,
-        filter=lambda record: record["level"].no >= caplog.handler.level,
-        enqueue=False, # Set to 'True' if your test is spawning child processes.
-    )
-    yield caplog
-    logger.remove(handler_id)
-
-import pytest
-from loguru import logger
-
-@pytest.fixture
-def reportlog(pytestconfig):
-    logging_plugin = pytestconfig.pluginmanager.getplugin("logging-plugin")
-    handler_id = logger.add(logging_plugin.report_handler, format="{message}")
-    yield
-    logger.remove(handler_id)
 
